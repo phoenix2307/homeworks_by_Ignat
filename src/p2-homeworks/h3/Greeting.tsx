67f47f319fe76ab388bd355e00ec2807d6095406
Alex <phoenix.trade2307@gmail.com>
2021-12-24 23:45:49 +0200

    completed

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -15,13 +15,19 @@ const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
     const inputClass = error === 'error' ? s.error : ''// need to fix with (?:) +
+    const errorClass = error === 'error' ? s.error : ''
+    const activeButton =  error === 'error'
 
     return (
         <div>
             <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
+
+            <button onClick={addUser} disabled={activeButton}>add</button>
+
             <span>{totalUsers}</span>
+            <div>
+                <span className={s.errorClass}>{error}</span>
+            </div>
         </div>
     )
 }
